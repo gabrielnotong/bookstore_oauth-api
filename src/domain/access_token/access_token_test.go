018575f0531b00b7fe9a353e8d00d4773ac21b38
gabrielnotong <gabrielnotong@gmail.com>
2020-04-19 22:11:17 +0200

    Test refactor using stretchr assert

--- src/domain/access_token/access_token_test.go
+++ src/domain/access_token/access_token_test.go
@@ -1,40 +1,26 @@
 package access_token
 
 import (
+	"github.com/stretchr/testify/assert"
 	"testing"
 	"time"
 )
 
 func TestExpirationDate(t *testing.T) {
-	if expirationDate != 24 {
-		t.Error("Expiration date should be 24")
-	}
+	assert.True(t, expirationDate == 24, "Expiration date should be 24")
 }
 
 func TestNewAccessToken(t *testing.T) {
 	at := NewAccessToken()
-	if at.IsExpired() {
-		t.Error("New access token should not be expired")
-	}
-
-	if at.ID != "" {
-		t.Error("New access token should not have ID")
-	}
-
-	if at.UserId != 0 {
-		t.Error("New access token should not have user id attached to it")
-	}
+	assert.False(t, at.IsExpired(), "New access token should not be expired")
+	assert.EqualValues(t, "", at.ID, "New access token should not have ID")
+	assert.False(t, at.UserId != 0, "New access token should not have user id attached to it")
 }
 
 func TestAccessTokenIsExpired(t *testing.T) {
 	at := &AccessToken{}
-
-	if !at.IsExpired() {
-		t.Error("Access token should be expired")
-	}
+	assert.True(t, at.IsExpired(), "Access token should be expired")
 
 	at.Expires = time.Now().UTC().Add(3 * time.Hour).Unix()
-	if at.IsExpired() {
-		t.Error("Access token should expire after 24 hours")
-	}
+	assert.False(t, at.IsExpired(), "Access token should expire after 24 hours")
 }
